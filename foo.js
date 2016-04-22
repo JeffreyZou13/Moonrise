8ebe396dcfacf5902546cdcaa79219108ded2458
Samuel Zhang <samuelzhang3@gmail.com>
2016-04-22 10:15:58 -0400

    spooky

--- foo.js
+++ foo.js
@@ -24,8 +24,8 @@ var makeLocation = function() {
 	right: true,
 	up: true,
 	speed: 1,
-  setx:function(s) { this.x = s },
-  sety:function(s) { this.y = s },
+	setx:function(s) { this.x = s },
+	sety:function(s) { this.y = s },
 	goUp: function(){this.y = this.y + this.speed;},
 	goDown: function(){this.y = this.y - this.speed;},
 	goRight: function(){this.x = this.x + this.speed;},
@@ -33,8 +33,10 @@ var makeLocation = function() {
     }
 };
 
-dvd = makeLocation();
-
+var index = 0;
+var arrayOfDvds = new Array();
+var dvd = arrayOfDvds[ index ];
+    
 var clear = function(){
     ctx.fillStyle = "#66cccc";
     ctx.fillRect( 0, 0, 500, 500 );
@@ -42,30 +44,42 @@ var clear = function(){
 };
 
 function go() {
-	if( dvd.x <= 0 || dvd.x >= 400 ) {
+    window.cancelAnimationFrame( requestId );
+
+    dvd = arrayOfDvds[ index ];
+    console.log( index );
+    console.log( dvd );
+
+    if( dvd.x <= 0 || dvd.x >= 400 )
 	    dvd.right = !dvd.right;
-    }
-	if( dvd.y <= 0 || dvd.y >= 450 ) {
-	    dvd.down = !dvd.down;
-      console.log(dvd.y);
-    }
+    
+    if( dvd.y <= 0 || dvd.y >= 450 ) 
+	dvd.up = !dvd.up;
+    
 
-  if( dvd.right ) {
-    dvd.setx(dvd.x + dvd.speed);
-  }
-	else {
-		dvd.setx(dvd.x - dvd.speed);
-  }
-	if( dvd.up ) {
-		dvd.sety(dvd.y + dvd.speed);
-  }
-	else {
-		dvd.sety(dvd.y - dvd.speed);
-  }
-
-	ctx.drawImage( logo, dvd.x, dvd.y, 100, 50 );
-	clear();
-	requestId = window.requestAnimationFrame( go );
+    if( dvd.right ) 
+	dvd.setx(dvd.x + dvd.speed);
+    
+    else 
+	dvd.setx(dvd.x - dvd.speed);
+    
+    if( dvd.up ) 
+	dvd.sety(dvd.y + dvd.speed);
+    
+    else 
+	dvd.sety(dvd.y - dvd.speed);
+    
+//    console.log(dvd.speed);
+    //    console.log( dvd.x );
+  //  console.log( dvd.y );
+    clear();
+    if( index < arrayOfDvds.length - 1)
+	index++;
+    else
+	index = 0;
+    
+    ctx.drawImage( logo, dvd.x, dvd.y, 100, 50 );
+    requestId = window.requestAnimationFrame( go );
 }
 
 function stop() {
@@ -75,8 +89,36 @@ function stop() {
     }
 }
 
+function faster() {
+    if(requestId) {
+	dvd.speed++;
+    }
+}
+
+function slower() {
+    if(requestId) {
+	if( dvd.speed > 1 )
+	    dvd.speed--;
+    }
+}
+
+function birth() {
+    var newDvd = makeLocation();
+    arrayOfDvds.push( newDvd );
+    console.log( arrayOfDvds );
+}
+
 var move = document.getElementById('go')
 move.addEventListener( "click", go );
 
 var stopBtn = document.getElementById( "stop" );
 stopBtn.addEventListener( "click", stop );
+
+var fasterBtn = document.getElementById( "faster" );
+fasterBtn.addEventListener( "click", faster );
+
+var slowerBtn = document.getElementById( "slower" );
+slowerBtn.addEventListener( "click", slower );
+
+var birthBtn = document.getElementById( "birth" );
+birthBtn.addEventListener( "click", birth );
